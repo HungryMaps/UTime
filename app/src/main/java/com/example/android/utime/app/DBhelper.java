502c749a2b559b34f0fe31a111d9a7fc275c9e3c
jennlv <jennylv17@gmail.com>
2014-10-02 18:37:00 -0600

    Cambios en eliminar y modificar REFACTOR 1

--- app/src/main/java/com/example/android/utime/app/DBhelper.java
+++ app/src/main/java/com/example/android/utime/app/DBhelper.java
@@ -44,7 +44,6 @@ public class DBhelper extends SQLiteOpenHelper {
      */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
         db.execSQL("DROP TABLE IF EXISTS " + Curso.TABLE);
         onCreate(db);
     }
