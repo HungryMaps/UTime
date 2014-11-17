850245b6ed74bbfd5ea4a8fcc02ae15a2c322468
Joan Marchena <jmarchena93@gmail.com>
2014-11-16 18:25:25 -0600

    Cambios Sugeridos

--- app/src/main/java/com/example/android/utime/app/Archivos.java
+++ app/src/main/java/com/example/android/utime/app/Archivos.java
@@ -4,26 +4,17 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.android.utime.app.R;
 
 import java.io.File;
 import java.io.FileFilter;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -70,7 +61,6 @@ public class Archivos extends ListActivity implements android.view.View.OnClickL
             public void onItemClick(AdapterView<?> parent, final View view,
                                     int position, long id) {
             }
-
         });
     }
 
@@ -97,6 +87,8 @@ public class Archivos extends ListActivity implements android.view.View.OnClickL
         }
         return super.onOptionsItemSelected(item);
     }
+
+    // Adapter usado para agregar a la lista, hereda de Array Adapter
 
     private class StableArrayAdapter extends ArrayAdapter<String> {
 
