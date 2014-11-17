1c3fc53eb7efa73ba5984c9526cbf3f035c13f0a
Joan Marchena <jmarchena93@gmail.com>
2014-11-16 18:17:07 -0600

    Revert "Cambios Archivos"
    This reverts commit 09c7615a2d56a22bcb0267eb51ce4350c041d4b2.

--- app/src/main/java/com/example/android/utime/app/Archivos.java
+++ app/src/main/java/com/example/android/utime/app/Archivos.java
@@ -1,21 +1,10 @@
 package com.example.android.utime.app;
 
-import android.app.ListActivity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.utime.app.R;
@@ -24,15 +13,11 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 // Activity para hacer el manejo de archivos
 
-public class Archivos extends ListActivity implements android.view.View.OnClickListener {
+public class Archivos extends ActionBarActivity {
 
-    TextView archivo_id;
     /*
     * Se obtienen los archivos del almacenamiento externo y se filtran
     * */
@@ -51,35 +36,17 @@ public class Archivos extends ListActivity implements android.view.View.OnClickL
             }
         };
 
-        ListView listView = getListView();
         File sdDir = Environment.getExternalStorageDirectory();
         String path = sdDir.getPath()+"/Download";
         File descargasDir = new File(path);
         File[] lista = descargasDir.listFiles(filtro);
-
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < lista.length; ++i) {
-            list.add(lista[i].getName());
+        for(int i=0; i<lista.length; i++){
+            Toast.makeText(this, lista[i].getName(), Toast.LENGTH_LONG).show();
         }
-
-        final StableArrayAdapter adapter = new StableArrayAdapter(this,android.R.layout.simple_list_item_1, list);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, final View view,
-                                    int position, long id) {
-            }
-
-        });
     }
 
 
     @Override
-    public void onClick(View view) {
-    }
-
-        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.archivos, menu);
@@ -96,30 +63,5 @@ public class Archivos extends ListActivity implements android.view.View.OnClickL
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    private class StableArrayAdapter extends ArrayAdapter<String> {
-
-        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-
-        public StableArrayAdapter(Context context, int textViewResourceId,
-                                  List<String> objects) {
-            super(context, textViewResourceId, objects);
-            for (int i = 0; i < objects.size(); ++i) {
-                mIdMap.put(objects.get(i), i);
-            }
-        }
-
-        @Override
-        public long getItemId(int position) {
-            String item = getItem(position);
-            return mIdMap.get(item);
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
     }
 }
